58a915481749b9fa0133402f8bf0bb72274a06c3
HugoNicolau <nicolau.hugogiles@gmail.com>
2023-02-14 13:11:10 -0300

    feat: alter port for deploy

--- src/index.js
+++ src/index.js
@@ -12,4 +12,5 @@ app.use(cors());
 app.use(authRoutes);
 app.use(balanceRoutes);
 
-app.listen(5000, () => console.log("Server running at port 5000 :)"));
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`Server running at port ${port} :)`));
